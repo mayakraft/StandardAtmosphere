baeaabbbc8bbc54775e423b07deb07e95e519117
robbykraft <robbykraft@gmail.com>
2015-04-02 16:43:28 -0500

    javascript

--- standardAtmosphere.js
+++ standardAtmosphere.js
@@ -2,33 +2,29 @@
 // robby kraft
 // mit open source software license
 
-#define EARTH_RADIUS 6371000.0   // meters
-#define REAL_GAS_CONSTANT 287.04 // earth air, m^2/Ksec^2
-#define E 2.71828182845904523536028747135266250
-#define METERS_TO_FEET 3.28083989501312
-#define HPA_TO_PSI 0.014503773773
-
+var EARTH_RADIUS = 6371000.0;   // meters
+var REAL_GAS_CONSTANT = 287.04 // earth air, m^2/Ksec^2
+var E = 2.71828182845904523536028747135266250;
+var METERS_TO_FEET = 3.28083989501312;
+var HPA_TO_PSI = 0.014503773773;
 // CONDITIONS AT ALTITUDE 0
-#define SEA_LEVEL_PRESSURE 1013.25
-#define SEA_LEVEL_TEMPERATURE 15
-#define SEA_LEVEL_GRAVITY 9.80665
-#define SEA_LEVEL_DENSITY 1.225
-#define SEA_LEVEL_SPEED_OF_SOUND 340.294
+var SEA_LEVEL_PRESSURE = 1013.25;
+var SEA_LEVEL_TEMPERATURE = 15;
+var SEA_LEVEL_GRAVITY = 9.80665;
+var SEA_LEVEL_DENSITY = 1.225;
+var SEA_LEVEL_SPEED_OF_SOUND = 340.294;
 
-#include <math.h>
-
-typedef struct atmosphere atmosphere;
-struct atmosphere{
-    double  temperature;    // celsius (288.15 in K)
-    double  pressure;       // psi  (101325 N/m^2) or (1013.25 hPa)
-    double  density;        // kg/m^3
-    double  gravity;        // m/sec^2
-    double  speed_of_sound; // m/sec
+function Atmosphere(){
+    var temperature;    // celsius (288.15 in K)
+    var pressure;       // psi  (101325 N/m^2) or (1013.25 hPa)
+    var density;        // kg/m^3
+    var gravity;        // m/sec^2
+    var speed_of_sound; // m/sec
 };
 
 // altitude in meters
-atmosphere atmosphereAtAltitude(double altitude){
-    struct atmosphere a;
+function atmosphereAtAltitude(altitude){
+    var a = new Atmosphere();
     if(altitude < 0.0 || altitude > 20000.0) return a;   // calculations only valid between sea level and 20,000m
     a.gravity = SEA_LEVEL_GRAVITY * pow( EARTH_RADIUS / (EARTH_RADIUS+altitude), 2);
     if(altitude < 11000.0){ // meters, (36,089 ft)
@@ -46,7 +42,7 @@ atmosphere atmosphereAtAltitude(double altitude){
 }
 
 // altitude in meters
-double speedOfSoundAtAltitude(double altitude){
+function speedOfSoundAtAltitude(altitude){
     if(altitude < 0.0 || altitude > 20000.0)
         return -1;
     else if(altitude < 11000.0)
@@ -56,12 +52,12 @@ double speedOfSoundAtAltitude(double altitude){
 }
 
 // altitude in meters
-double gravityAtAltitude(double altitude){
+function gravityAtAltitude(altitude){
     return SEA_LEVEL_GRAVITY * pow( EARTH_RADIUS / (EARTH_RADIUS+altitude), 2);
 }
 
 // altitude in meters
-double temperatureAtAltitude(double altitude){
+function temperatureAtAltitude(altitude){
     if(altitude < 0.0 || altitude > 20000.0)
         return -1;
     else if(altitude < 11000.0)
@@ -71,7 +67,7 @@ double temperatureAtAltitude(double altitude){
 }
 
 // altitude in meters
-double pressureAtAltitude(double altitude){
+function pressureAtAltitude(altitude){
     if(altitude < 0.0 || altitude > 20000.0)
         return -1;
     else if(altitude < 11000.0)
@@ -81,10 +77,10 @@ double pressureAtAltitude(double altitude){
 }
 
 // altitude in meters
-double densityAtAltitude(double altitude){
-    double temperature = SEA_LEVEL_TEMPERATURE;
-    double pressure = SEA_LEVEL_PRESSURE;
-    double gravity = SEA_LEVEL_GRAVITY;
+function densityAtAltitude(altitude){
+    var temperature = SEA_LEVEL_TEMPERATURE;
+    var pressure = SEA_LEVEL_PRESSURE;
+    var gravity = SEA_LEVEL_GRAVITY;
     if(altitude < 0.0 || altitude > 20000.0)
         return -1;
     else if(altitude < 11000.0){
